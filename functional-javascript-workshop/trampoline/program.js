7fe78d6c7cc334c22925763eecf54a202727d3f8
jjcody <jackcody@gmail.com>
2014-11-01 16:17:44 -0700

    fixed trampoline

--- functional-javascript-workshop/trampoline/program.js
+++ functional-javascript-workshop/trampoline/program.js
@@ -1,21 +1,20 @@
+// For an explanation of the trampoline concept see:
+// http://www.integralist.co.uk/posts/understanding-recursion-in-functional-javascript-programming/
 // http://stephen-young.me.uk/2013/01/20/functional-programming-with-javascript.html
-// http://taylodl.wordpress.com/2013/06/07/functional-javascript-tail-call-optimization-and-trampolines/
 
 function repeat(operation, num) {
-  // Modify this so it doesn't cause a stack overflow!
   if (num <= 0) return
   operation()
-  return trampoline(repeat.bind(null, operation, --num))
+  return repeat.bind(null, operation, --num)
 }
 
 function trampoline(fn) {
 	while(fn && fn instanceof Function) {
-		fn = fn.apply(fn.context, fn.args)
+    fn = fn()
 	}
 	return fn
 }
 
 module.exports = function(operation, num) {
-  // You probably want to call your trampoline here!
-  trampoline(repeat.bind(null, operation, num))
+  return trampoline(repeat.bind(null, operation, num))
 }
